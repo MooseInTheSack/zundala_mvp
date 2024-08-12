33b8a587aeb53c140bc4fcc2e7136189c01cf980
MooseInTheSack <tyl3rkirkpatrick@yahoo.com>
2024-08-11 22:08:06 -0500

    added library to handle form input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import BasicTextFields from "./components/TextBox/TextBox";
-import Date from "./components/Date/Date";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { DatePickerField } from "./components/DatePicker/DatePicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
-  const [dataFromChild, setDataFromChild] = useState("");
-
-  function handleDataFromChild(data) {
-    console.log("ducky data: ", data);
-    setDataFromChild(data);
-  }
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <BasicTextFields sendDataToParent={handleDataFromChild} />
-
-        <Date />
+        <div>
+          <h1>Any place in your app!</h1>
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validate={(values) => {
+              const errors = {};
+              if (!values.email) {
+                errors.email = "Required";
+              } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+              ) {
+                errors.email = "Invalid email address";
+              }
+              return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {({ isSubmitting }) => (
+              <Form>
+                <Field type="email" name="email" />
+                <ErrorMessage name="email" component="div" />
+                <Field type="password" name="password" />
+                <DatePickerField name="date" />
+                <ErrorMessage name="password" component="div" />
+                <button type="submit" disabled={isSubmitting}>
+                  Submit
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </div>
       </header>
     </div>
   );
